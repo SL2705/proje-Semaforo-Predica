1740848212dac9627d6756052cc8abedb6067da9
Daniel <al26120162@utcj.edu.mx>
2026-02-25 22:41:21 -0700

    Corrección de importaciones y variables en login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,20 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { 
+  IonContent, 
+  IonHeader, 
+  IonTitle, 
+  IonToolbar, 
+  IonItem, 
+  IonInput, 
+  IonButton, 
+  IonIcon, // <--- IMPORTANTE: Esto le falta a Leo
+  IonLabel 
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent, 
+    IonHeader, 
+    IonTitle, 
+    IonToolbar, 
+    CommonModule, 
+    FormsModule,
+    IonItem,
+    IonInput,
+    IonButton,
+    IonIcon, 
+    IonLabel
+  ]
 })
 export class LoginPage implements OnInit {
+  
+  showPassword = false; 
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  login() {
+    console.log('Intento de login');
+  }
 }
