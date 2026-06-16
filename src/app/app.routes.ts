1e08e7bc4af0bc445d719649a64755063872698a
SsL27 <leovictori2005@gmail.com>
2026-06-15 19:07:02 -0600

    Add files via upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,37 +1,79 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
+    redirectTo: 'login',
+    pathMatch: 'full'
   },
+
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./home/home.page').then(m => m.HomePage)
+  },
+
   {
     path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+    loadComponent: () =>
+      import('./pages/login/login.page').then(m => m.LoginPage)
   },
-  {
-    path: 'operador',
-    loadComponent: () => import('./pages/operador/operador.page').then( m => m.OperadorPage)
-  },
+
   {
     path: 'registro',
-    loadComponent: () => import('./pages/registro/registro.page').then( m => m.RegistroPage)
+    loadComponent: () =>
+      import('./pages/registro/registro.page').then(m => m.RegistroPage)
   },
+
   {
-    path: 'jefe-grupo',
-    loadComponent: () => import('./pages/jefe-grupo/jefe-grupo.page').then( m => m.JefeGrupoPage)
+    path: 'kardex/:reloj',
+    canActivate: [AuthGuard],
+    data: { roles: ['operador', 'supervisor'] },
+    loadComponent: () =>
+      import('./kardex/kardex.page').then(m => m.KardexPage)
   },
+
+  {
+    path: 'operador',
+    canActivate: [AuthGuard],
+    data: { roles: ['operador'] },
+    loadComponent: () =>
+      import('./pages/operador/operador.page').then(m => m.OperadorPage)
+  },
+
   {
     path: 'supervisor',
-    loadComponent: () => import('./pages/supervisor/supervisor.page').then( m => m.SupervisorPage)
+    canActivate: [AuthGuard],
+    data: { roles: ['supervisor'] },
+    loadComponent: () =>
+      import('./pages/supervisor/supervisor.page').then(m => m.SupervisorPage)
   },
+
   {
-    path: 'recursos-humanos',
-    loadComponent: () => import('./pages/recursos-humanos/recursos-humanos.page').then( m => m.RecursosHumanosPage)
+    path: 'rrhh',
+    canActivate: [AuthGuard],
+    data: { roles: ['rrhh'] },
+    loadComponent: () =>
+      import('./pages/recursos-humanos/recursos-humanos.page')
+        .then(m => m.RecursosHumanosPage)
+  },
+
+  {
+    path: 'jefe',
+    canActivate: [AuthGuard],
+    data: { roles: ['jefe'] },
+    loadComponent: () =>
+      import('./pages/jefe-grupo/jefe-grupo.page')
+        .then(m => m.JefeGrupoPage)
+  },
+
+  {
+    path: 'sistemas',
+    canActivate: [AuthGuard],
+    data: { roles: ['sistemas'] },
+    loadComponent: () =>
+      import('./pages/sistemas/sistemas.page')
+        .then(m => m.SistemasPage)
   }
 ];
